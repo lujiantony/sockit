90b5073d9406a44edbb3dc7a2eb7512987a02a4a
root <root@localhost.localdomain>
2013-12-04 19:15:27 -0500

    chat_server.py initial commit

--- Lab2/src/chat_server.py
+++ Lab2/src/chat_server.py
@@ -1,8 +1,11 @@
-# Tcp Chat server
+#Chat server used for CSEE 4840 @ Columbia University
+#Jian Lu
+#Columbia University SEAS'13
+#jl3946@columbia.edu
  
-import socket, select
+import socket, select, struct, fcntl
  
-#Function to broadcast chat messages to all connected clients
+#This function is used to broadcast chat messages to all connected clients
 def broadcast_data (sock, message):
     #Do not send the message to master socket and the client who has send us the message
     for socket in CONNECTION_LIST:
@@ -13,24 +16,33 @@ def broadcast_data (sock, message):
                 # broken socket connection may be, chat client pressed ctrl+c for example
                 socket.close()
                 CONNECTION_LIST.remove(socket)
+
+#This function is used to get the ip address the local host
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+	struct.pack('256s', ifname[:15]))[20:24])
  
 if __name__ == "__main__":
      
     # List to keep track of socket descriptors
     CONNECTION_LIST = []
     RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = 5000
-     
+    PORT = 42000
+    host = ''; 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this has no effect, why ?
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.bind((host, PORT))
     server_socket.listen(10)
  
     # Add server socket to the list of readable connections
     CONNECTION_LIST.append(server_socket)
  
-    print "Chat server started on port " + str(PORT)
+    print "CHAT SERVER START ON " + str(get_ip_address('eth0')) + ":" + str(PORT) 
+    print "READY TO RECEIVE CONNECTIONS..."
  
     while 1:
         # Get the list sockets which are ready to be read through select
@@ -43,7 +55,8 @@ if __name__ == "__main__":
                 sockfd, addr = server_socket.accept()
                 CONNECTION_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
-                 
+                
+		sockfd.send("#Welcome to CSEE 4840 Lab2!#") 
                 broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
              
             #Some incoming message from a client
@@ -54,7 +67,8 @@ if __name__ == "__main__":
                     # a "Connection reset by peer" exception will be thrown
                     data = sock.recv(RECV_BUFFER)
                     if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)                
+                        broadcast_data(sock, '<' + str(sock.getpeername()) + '> ' + data) 
+			print "RECEIVED: " + data 	               
                  
                 except:
                     broadcast_data(sock, "Client (%s, %s) is offline" % addr)
